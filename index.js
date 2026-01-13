f8931147d85f06ceb8070af3192561afabd3bbbf
Informatica <informatica@alumno.local>
2026-01-13 07:43:43 -0600

    001 - Archivos iniciales

---
+++ index.js
@@ -0,0 +1,10 @@
+let root = document.getElementById("root");
+ 
+root.innerHTML = "<h1>Hola mundo</h1>";
+ 
+let h1 = document.createElement("h1");
+h1.innerHTML = "Etiquetas para texto";
+root.appendChild(h1);
+
+// Agregar una lista de elementos html para texto
+
